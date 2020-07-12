f015eadd9ae304fb2812a544e5659cc4ddda4dcf
Conor Barnes <conorbarnes93@gmail.com>
2020-07-12 11:29:08 -0700

    Reformat dates in metadata

--- exceltweak.py
+++ exceltweak.py
@@ -1,5 +1,6 @@
 from openpyxl import Workbook
 from openpyxl import load_workbook
+import re
 import sys
 
 # if len(sys.argv) != 3:
@@ -17,6 +18,7 @@ print(ws['B1'].value)
 metadataKeeps = ['Report_Name', 'Report_ID', 'Reporting_Period', 'Created']
 metadataGoodbyes = []
 
+# Find the rows we don't want
 for count, row in enumerate(ws.rows, 1):
     # Reach the end of the metadata section
     if row[0].value == None:
@@ -27,10 +29,19 @@ for count, row in enumerate(ws.rows, 1):
 
 print(metadataGoodbyes)
 
+# Purge the rows we don't want
 for row in reversed(metadataGoodbyes):
     ws.delete_rows(row)
 
+# Reformat the dates in the remaining rows.
+rp = ws['B3'].value
+rp = re.sub(r'[\w_]+?=', '', rp)
+rp = re.sub(';', ' to', rp)
+ws['B3'].value = rp
 
+cr = ws['B4'].value
+cr = re.sub('T.*', '', cr)
+ws['B4'].value = cr
 
 
 # End
