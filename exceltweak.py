1818baadf67ddf32d3e8a977b035c2c080192cff
Conor Barnes <conorbarnes93@gmail.com>
2020-07-13 09:44:46 -0700

    Fix cell styles, change files used to be command line args

--- exceltweak.py
+++ exceltweak.py
@@ -1,5 +1,5 @@
 from openpyxl import Workbook, load_workbook
-from openpyxl.styles import Font
+from openpyxl.styles import Font, PatternFill
 import string
 import re
 import sys
@@ -53,7 +53,6 @@ def tablesplit(ws):
     print('tablesplit!')
     metricColumn = findMetric(ws)
     for row in ws.iter_rows(min_col = 1, min_row = tablerow, max_col = columnCount, max_row = rowCount):
-        print(row)
         if row[metricColumn].value == 'Unique_Item_Requests':
             continue
         else:
@@ -66,15 +65,25 @@ def tablesplit(ws):
                 ws.delete_rows(cell.row)
 
 
+# BEGIN
 
-# if len(sys.argv) != 3:
-#    print("Please include the name of the file being tweaked and a file name where the output should go")
+if len(sys.argv) != 3:
+   print(sys.argv)
+   sys.exit("Please include the name of the file being tweaked and a file name where the output should go")
 
-# wb = load_workbook(sys.argv[1])
+wb = load_workbook(sys.argv[1])
 
-wb = load_workbook('TR_B1_Input.xlsx')
+# wb = load_workbook('TR_B1_Input.xlsx')
 ws = wb.active
 wbtype = ws['B2'].value
+
+# Initial styles
+
+for row in ws.iter_rows(min_col = 1, min_row = 1, max_col = ws.max_column, max_row = ws.max_row):
+    for cell in row:
+        cell.font = Font(name = 'Calibri', size = 12)
+
+
 
 # METADATA SECTION
 
@@ -84,8 +93,11 @@ metadataGoodbyes = []
 # Find the rows we don't want
 for count, row in enumerate(ws.rows, 1):
     # Reach the end of the metadata section
+    
     if row[0].value == None:
         break
+    for cell in row:
+        cell.font = Font(name = 'Calibri', size = 12, bold=True)
     if row[0].value not in metadataKeeps:
         metadataGoodbyes.append(count)
 
@@ -102,14 +114,6 @@ ws['B3'].value = rp
 cr = ws['B4'].value
 cr = re.sub('T.*', '', cr)
 ws['B4'].value = cr
-
-# Style metadata area (do when reinserted, actually)
-
-# for row in ws.iter_rows(min_row = 1, max_col = 2, max_row = 4): 
-#     for cell in row:
-#         cell.font = Font(name = 'Calibri', size = 12, bold = True)
-        # background color...
-
 
 
 #DATA SECTION
@@ -135,12 +139,12 @@ dataGoodbyeColumns = []
 
 
 # find header row of table to be used through rest of program as variable
-
 tablerow = ''
 for count, row in enumerate(ws.iter_rows(min_col=1, min_row=1, max_col=1, max_row=25, values_only=True), 1):
     for cell in row:
         if cell == 'Title':
             tablerow = count
+            break
 
 
 
@@ -163,20 +167,25 @@ for column in reversed(dataGoodbyeColumns):
 for row in ws.iter_rows(min_col=2, min_row=1, max_col=2, max_row=4):
     for cell in row: 
         cell.value = keepMetadatas.pop(0)
-        cell.font = Font(name = 'Calibri', size = 12, bold = True)
+        cell.font = Font(name = 'Calibri', bold = True)
+
+# color metadata
+for row in ws.iter_rows(min_col = 1, min_row = 1, max_col = 5, max_row = 3):
+    for cell in row:
+        if cell.row == 3:
+            cell.fill = PatternFill(start_color='FF9999', fill_type='solid')
+        else:
+            cell.fill = PatternFill(start_color='CCFF99', fill_type='solid')
 
 # find the 'total' column and move it to the end. Along the way, change the titles of columns. 
 origcolumn = ''
 movecolumn = ''
 breaker = 0
-monthcheck = 0
 
-# + 1 to give space for the reinsertion
-columnCount = ws.max_column + 1
+columnCount = ws.max_column + 1 # + 1 to give space for the reinsertion
 for count, column in enumerate(ws.iter_cols(min_col=1, min_row=tablerow, max_col=columnCount, max_row=tablerow, values_only=True), 1):
     for cell in column:
         if cell == 'Reporting_Period_Total':
-            # cell = 'YTD Total'
             origcolumn = count
             columnletter = string.ascii_uppercase[origcolumn - 1]
         if cell == None:
@@ -199,24 +208,31 @@ for column in ws.iter_cols(min_col = 1, min_row = tablerow, max_col = columnCoun
     for cell in column:
         cell.value = cell.value.replace('-2020', '')
         cell.value = cell.value.replace('Reporting_Period_Total', 'YTD Total')
+        cell.font = Font(color = '000000', name = 'Calibri', bold=True, size = 12)
+        cell.fill = PatternFill(start_color='99CCFF', fill_type='solid')
 
 
-# add row of sums! And bold them! 
+# add row of sums! 
 ws.insert_rows(tablerow + 1)
+sumRow = ws.row_dimensions[tablerow + 1]
+sumRow.font = Font(name = 'Calibri', size = 12, bold = True)
+sumRow.fill = PatternFill(start_color='99CCFF', fill_type='solid')
+
 rowCount = ws.max_row # henceforth, this is how tall the table is!
 
 for count, column in enumerate(ws.iter_cols(min_col = origcolumn, min_row = tablerow + 1, max_col = columnCount, max_row = tablerow), 5):
     columnletter = string.ascii_uppercase[count]
     cellcode = f'{columnletter}{tablerow + 1}'
     ws[cellcode].value = f"=SUM({columnletter}{tablerow + 2}:{columnletter}{rowCount})"
-    ws[cellcode].font = Font(name = 'Calibri', size = 12, bold = True)
+    ws[cellcode].font = Font(name = 'Calibri', bold = True, size = 12)
+    ws[cellcode].fill = PatternFill(start_color='99CCFF', fill_type='solid')
 
-# add 'title' to first cell in that row
+# add 'Total' to first cell in that row
 ws[f'A{tablerow + 1}'].value = 'Total'
-ws[f'A{tablerow + 1}'].font = Font(name = 'Calibri', size = 12, bold = True)
+ws[f'A{tablerow + 1}'].font = Font(name = 'Calibri', bold = True, size = 12)
+ws[f'A{tablerow + 1}'].fill = PatternFill(start_color='99CCFF', fill_type='solid')
 
 # separate totals and uniques
-
 if (rowCount - tablerow) > 20:
     sheetsplit(ws)
 else: 
@@ -224,5 +240,6 @@ else:
 
 
 # END
-#wb.save(sys.argv[2])
-wb.save('Output.xlsx')
+
+wb.save(sys.argv[2])
+# wb.save('Output.xlsx')
